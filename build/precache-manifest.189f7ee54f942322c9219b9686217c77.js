047884579e26e172c93b5eef6544ddd1ae367891
Pravin kanna P <mp.pravinkanna@gmail.com>
2020-08-28 08:56:34 +0530

    added build dir

---
+++ build/precache-manifest.189f7ee54f942322c9219b9686217c77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c329c0aa2affa16e7e526ddc7353e5",
+    "url": "/algorithms-visualizer/index.html"
+  },
+  {
+    "revision": "2cc62b08f295af4554cf",
+    "url": "/algorithms-visualizer/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "cdc534c54661f8a60e2d",
+    "url": "/algorithms-visualizer/static/css/main.9e55862f.chunk.css"
+  },
+  {
+    "revision": "2cc62b08f295af4554cf",
+    "url": "/algorithms-visualizer/static/js/2.dab8e8f2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/algorithms-visualizer/static/js/2.dab8e8f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc534c54661f8a60e2d",
+    "url": "/algorithms-visualizer/static/js/main.135a4b06.chunk.js"
+  },
+  {
+    "revision": "0480c75e1d87d270f057",
+    "url": "/algorithms-visualizer/static/js/runtime-main.d06e165b.js"
+  },
+  {
+    "revision": "4d68cf84177931050a1f4f073d2ef7e7",
+    "url": "/algorithms-visualizer/static/media/logo.4d68cf84.svg"
+  }
+]);
